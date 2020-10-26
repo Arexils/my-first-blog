d67c6eaab772b75ef6f6a7367b98b4922e43391e
Arexils <20801433+Arexils@users.noreply.github.com>
2020-10-26 23:41:47 +0300

    Added views to create/edit blog post inside the site.

--- Django/settings.py
+++ Django/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'blog/../templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
